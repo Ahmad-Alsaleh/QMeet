18021ff4a9130f0bf1bfa937103d582269ff3c43
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-05-23 08:52:45 +0400

    feat: add tray, shortcut listener, and quit features

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+use enigo::{Enigo, Keyboard as _};
+use std::error::Error;
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
 pub fn run() {
+    let target_shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::ALT), Code::KeyP);
+    let meeting_url = generate_meeting_url();
+
     tauri::Builder::default()
-        .setup(|app| {
+        .plugin(tauri_plugin_process::init())
+        .setup(move |app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
@@ -9,8 +20,58 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
+            register_shortcut_listener(app, target_shortcut, meeting_url)?;
+
+            let quit_menu_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_menu_item])?;
+
+            let icon = app.default_window_icon().unwrap().clone();
+            TrayIconBuilder::new()
+                .icon(icon)
+                .menu(&menu)
+                .show_menu_on_left_click(true)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => app.exit(0),
+                    _ => unreachable!(),
+                })
+                .build(app)?;
+
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn register_shortcut_listener(
+    app: &mut tauri::App,
+    target_shortcut: Shortcut,
+    meeting_url: String,
+) -> Result<(), Box<dyn Error>> {
+    app.handle().plugin(
+        tauri_plugin_global_shortcut::Builder::new()
+            .with_handler(move |_app, shortcut, event| {
+                if shortcut == &target_shortcut && matches!(event.state(), ShortcutState::Pressed) {
+                    println!("Shortcut Pressed");
+                    Enigo::new(&enigo::Settings::default())
+                        .unwrap()
+                        .text(&meeting_url)
+                        .expect("Couldn't paste meeting URL");
+                }
+            })
+            .build(),
+    )?;
+    app.global_shortcut().register(target_shortcut)?;
+    Ok(())
+}
+
+pub fn generate_meeting_url() -> String {
+    // TODO: this should use Google Meet API
+    String::from("meet.google.com/xio-xfkn-wsm")
+}
+
+#[tauri::command]
+fn my_custom_command() {
+    println!("my custom command");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+import { invoke } from '@tauri-apps/api/core';
+import { exit } from '@tauri-apps/plugin-process';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center px-4 py-8">
@@ -9,7 +13,7 @@ export default function Home() {
           <button className="button w-full sm:w-auto rounded-xl bg-blue-600 px-6 py-3 text-white shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 transition">
             Modify Shortcut
           </button>
-          <button className="button w-full sm:w-auto rounded-xl bg-red-500 px-6 py-3 text-white shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition">
+          <button onClick={async () => await exit(0)} className="button w-full sm:w-auto rounded-xl bg-red-500 px-6 py-3 text-white shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition">
             Quit App
           </button>
         </div>
